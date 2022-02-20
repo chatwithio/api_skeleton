12809ee0ea75198f19cfa3bfc233f0cd4983e3cf
ankurthakur211 <ankurthakur211@gmail.com>
2022-02-20 17:08:29 +0530

    worked on symfony project

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Service\MessageService;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;  
-use Symfony\Component\HttpKernel\KernelInterface; 
+
 
 class MessageController extends AbstractController
 {
@@ -30,7 +30,7 @@ class MessageController extends AbstractController
      */
     public function sendMessage()
     {
-        $sandBoxKey = $this->getParameter('whats-app-key')['API_KEY_360DEGREE'];
+        
 
       if ($this ->request ->request ->has('to') || $this ->request ->query->has('to'))
         {
@@ -73,4 +73,95 @@ class MessageController extends AbstractController
        
        
     }
+
+
+     /**
+     * @Route("/sendMessageTemplate", name="messageTemplate", methods={"POST"})
+     */
+
+
+     public function sendMessageTemplate()
+     {
+
+
+     if ($this ->request ->request ->has('to') || $this ->request ->query->has('to'))
+        {
+        $to = (string)$this->request->request->has('to') ? $this->request->request->get('to') : $this->request
+                ->query
+                ->get('to');
+           
+        }
+        else
+        {
+             throw new Exception('to parameter is required');
+        }
+
+
+     if ($this ->request ->request ->has('placeholders') || $this ->request ->query->has('placeholders'))
+        {
+        $placeholders = (string)$this->request->request->has('placeholders') ? $this->request->request->get('placeholders') : $this->request
+                ->query
+                ->get('placeholders');
+           
+        }
+        else
+        {
+             throw new Exception('placeholders parameter is required');
+        }
+
+
+      if ($this ->request ->request ->has('template') || $this ->request ->query->has('template'))
+         {
+        $template = (string)$this->request->request->has('template') ? $this->request->request->get('template') : $this->request
+                ->query
+                ->get('template');
+           
+         }
+        else
+         {
+             throw new Exception('template parameter is required');
+         }
+
+
+
+
+            if ($this ->request ->request ->has('language') || $this ->request ->query->has('language'))
+        {
+        $language = (string)$this->request->request->has('language') ? $this->request->request->get('language') : $this->request
+                ->query
+                ->get('language');
+           
+        }
+        else
+        {
+             throw new Exception('language parameter is required');
+        }
+
+
+              if ($this ->request ->request ->has('namespace') || $this ->request ->query->has('namespace'))
+        {
+        $namespace = (string)$this->request->request->has('namespace') ? $this->request->request->get('namespace') : $this->request
+                ->query
+                ->get('namespace');
+           
+        }
+        else
+        {
+             throw new Exception('namespace parameter is required');
+        }
+
+        $response = new JsonResponse();
+        $sendMessage=$this->messageService->sendWhatsApp($to, $placeholders, $template, $language, $namespace);
+
+               if($sendMessage->meta->api_status=='stable')
+        {
+
+         return $response->setData(['message' => "Message Sent Successfully"]);
+        }
+        else
+        {
+          return $response->setData(['message' => "Something Went Wrong"]);  
+        }
+
+     }
 }
