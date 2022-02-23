70eff7684508588da20479f9cabfb633d3f2f182
ward <wardazo@gmail.com>
2022-02-23 12:35:23 +0100

    Added api, postgres and user (2)

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+use function Symfony\Component\String\u;
+
+#[AsCommand(
+    name: 'app:create-update-user',
+    description: 'Created or updates a user',
+)]
+class CreateUserCommand extends Command
+{
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher,
+        private UserRepository $users
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::OPTIONAL, 'The email of the new user')
+            ->addArgument('password', InputArgument::OPTIONAL, 'The plain password of the new user')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $plainPassword = $input->getArgument('password');
+        $email = $input->getArgument('email');
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy([
+            "email"=>$email
+        ]);
+
+        if(empty($email) || empty($plainPassword)){
+            $io->error("You must include an email and password");
+            return Command::SUCCESS;
+        }
+
+        $text = "The user was updated";
+
+        if(!$user){
+            $user = new User();
+            $user->setEmail($email);
+            $user->setRoles(['ROLE_ADMIN']);
+            $text = "The user was created";
+        }
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success($text);
+
+        return Command::SUCCESS;
+    }
+}
