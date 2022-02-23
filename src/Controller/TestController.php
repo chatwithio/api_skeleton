1d4197804530c7e80925272a861dc805b01f9e4b
ward <wardazo@gmail.com>
2022-02-23 12:33:51 +0100

    Added api, postgres and user

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,8 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'test')]
+    // POST
     public function index(MessageBusInterface $bus): Response
     {
+
+        /*
+         * Json:
+         *
+         * "654765476547654",
+        ["011111","Ward","$12.00","Macbook"],
+        'order_sent',
+        'en',
+        'f6baa15e_fb52_4d4f_a5a0_cde307dc3a85'
+         *
+         *
+         *
+         *
+         */
+
+
+
+
         $bus->dispatch(new WhatsappNotification('Whatsapp me!'));
 
         return $this->json([
---
+++ src/Service/MessageService.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Service;
+
+use GuzzleHttp\Client;
+
+
+class MessageService
+{
+
+    private $senderEmail;
+
+    private $wa_id = false;
+
+    private array $env = [
+        'prod' => 'https://waba.360dialog.io',
+        'dev' => 'https://waba-sandbox.360dialog.io',
+
+    ];
+
+    //The api endpoints. There are more but these are the important ones
+    private array $endpoint = [
+        'contact' => [
+            'method' => 'POST',
+            'url' => '/v1/contacts'
+        ],
+        'message' => [
+            'method' => 'POST',
+            'url' => '/v1/messages'
+        ]
+        ,
+        'template' => [
+            'method' => 'GET',
+            'url' => '/v1/configs/templates'
+        ],
+    ];
+
+    //The header information. It contains the auth token too
+    private array $headers = [
+        'Content-Type' => 'application/json',
+        'D360-API-KEY' => null
+    ];
+
+
+    //False of the telephone number has KOed by whatsapp,
+    //eg. if the user does not have whatsapp or it is a malformed number
+    private bool $payloadOk = true;
+
+
+    /**
+     * WhatsApp360 constructor.
+     */
+    public function __construct()
+    {
+
+        $this->headers['D360-API-KEY'] = $_ENV['WHATSAPP_KEY'];
+
+        foreach ($this->endpoint as $k => $v) {
+            $this->endpoint[$k]['url'] = $this->env[$_SERVER['WHATSAPP_ENV']] . $this->endpoint[$k]['url'];
+        }
+    }
+
+    /**
+     * @param $endpoint (The endpoint to be used - see private $endpoint)
+     * @param array $data
+     * @return void (if there is any error an exception should be thrown)
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+
+    private function send($endpoint, $data = [])
+    {
+
+        try {
+            if ($this->payloadOk === true) {
+
+                $client = new Client;
+
+                $request = $client->request(
+                    $this->endpoint[$endpoint]['method'],
+                    $this->endpoint[$endpoint]['url'],
+                    [
+                        "headers" => $this->headers,
+                        "json" => $data
+                    ]
+                );
+
+                if ($request->getStatusCode() == 200 || $request->getStatusCode() == 201) {
+                    return json_decode($request->getBody()->getContents());
+                } else {
+                    throw new Exception($request->getBody()->getContents());
+                }
+            } else {
+                throw new Exception('Unvalidated payload Exception');
+            }
+        } catch (Exception $exception) {
+            $this->logErrors($exception);
+        }
+    }
+
+
+
+
+    /**
+     * @param $contact (whatapp number with no + or spacing)
+     * @return bool (return true if the contact id OKed)
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+
+    public function checkContact($contact)
+    {
+        try {
+            $payload = [
+                "blocking" => "wait",
+                "contacts" => ["+" . $contact],
+                "force_check" => true
+            ];
+
+            $response = $this->send('contact', $payload);
+
+            if (!empty($response->contacts)) {
+                if (isset($response->contacts[0]) && isset($response->contacts[0]->status) && $response->contacts[0]->status == 'valid') {
+                    $this->wa_id = $response->contacts[0]->wa_id;
+                    return true;
+                }
+            }
+            return false;
+
+        } catch (Exception $exception) {
+            $this->logErrors($exception);
+        }
+    }
+
+    /**
+     * @param $placeholders (an array of text only placeholders)
+     * @return array
+     */
+
+    private function buildParams($placeholders)
+    {
+        $arr = [];
+        foreach ($placeholders as $placeholder) {
+            $arr[] = [
+                "type" => "text",
+                "text" => $placeholder
+            ];
+        }
+        return $arr;
+    }
+
+    /**
+     * @param $to (whatapp number  - no spaces or = )
+     * @param $placeholders array (of placeholders)
+     * @param $template string (template name)
+     * @param $language languale (ie en, must match the language of the approved template)
+     * @param $namespace  string (template namespace  - you can get this from the getTemplates api)
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * NOTE, a template must be approved before it can be used
+     */
+
+    public function sendWhatsApp($to, $placeholders, $template, $language, $namespace)
+    {
+        $this->checkContact($to);
+
+
+
+        $payload = [
+            "to" => $to,
+            "type" => "template",
+            "template" => [
+                "namespace" => $namespace,
+                "language" => [
+                    "policy" => "deterministic",
+                    "code" => $language
+                ],
+                "name" => $template,
+                "components" => [
+                    [
+                        "type" => "body",
+                        "parameters" => $this->buildParams($placeholders)
+
+                    ]
+                ]
+            ]
+        ];
+
+        return $this->send('message', $payload);
+    }
+
+    /**
+     * @return array (list of templates  - with their nmespaces and approved status)
+     */
+
+
+    public function sendWhatsAppText($to, $message)
+    {
+        $payload = [
+            "to" => $to,
+            "type" => "text",
+            "text" => ["body" => $message]
+
+        ];
+
+        return $this->send('message', $payload);
+    }
+
+
+    public function getTemplates()
+    {
+        return $this->send('template');
+    }
+
+    private function logErrors(Exception $exception)
+    {
+        print "ERROR LOGGED:" . $exception->getMessage() . "\n";
+    }
+}
+
+
