9da0e9339f2eacaaa05b266250fbc716a598e3bb
ankurthakur211 <ankurthakur211@gmail.com>
2022-02-24 19:43:06 +0530

    worked on moving code to notification handler

--- src/Controller/HookController.php
+++ src/Controller/HookController.php
@@ -16,9 +16,8 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Message;
-use App\Service\MessageService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\MessageHandler\WhatsappNotificationHandler;
 
 
 class HookController extends AbstractController
@@ -26,10 +25,11 @@ class HookController extends AbstractController
 
 
 
-  public function __construct(EntityManagerInterface $em, MessageService $service)
+  public function __construct(WhatsappNotificationHandler $notificationHandler)
     {
-   $this->messageService = $service;
-   $this->entityManager = $em;
+
+   $this->notificationHandler = $notificationHandler;
+
   
 
     }
@@ -85,45 +85,16 @@ class HookController extends AbstractController
     public function saveMessageData(Request $request)
     {
 
-        $randomNumber = rand(100000,999999);
-
-      $contact = '{"contact":{"profile":{"name":"ward"},"wa_id":"34622814642"},"message":{"from":"34622814642","id":"ABGGNGIoFGQvAgo-sAr3kcI5DI30","text":{"body":'.$randomNumber.'},"timestamp":"1640174341","type":"text"}}';
-
-      $data = json_decode($contact, true);
-     /* print '<pre>';
-      print_r($data);*/
-
-      $text = $data['message']['text']['body'];
-      $name =    $data['contact']['profile']['name'];
-      $phoneNumber = $data['message']['from'];
-      $time = $data['message']['timestamp'];
-      $type = $data['message']['type'];
-      $orderId = $data['message']['id'];
-
-
-
-      $message = new message();
-      $entityManager = $this->entityManager;
-      $message->setName($name);
-      $message->setMessage($text);
-      $message->setPhoneNumber($phoneNumber);
-      $message->setOrderId($orderId);
-      $message->setCreatedAt($time);
-      $message->setType($type);
-      $entityManager->persist($message);
-      $entityManager->flush();
-
-      $this->messageService->sendWhatsAppText("34622814642","Hi there");
-
-    
-
-        $response = new JsonResponse();
-        $response->setData(['data' => 'Data added successfully']);
-        return $response;
-
-
-    
-
+        $saveData = $this->notificationHandler->saveMessageData();
+  /*      print '<pre>';
+        print_r($saveData);
+        exit;*/
+        if( $saveData['status']=="true")
+        {
+            $response = new JsonResponse();
+            $response->setData(['message' => 'Data Saved Successfully']);
+            return $response;
+        }
 
     }
 
--- src/MessageHandler/WhatsappNotificationHandler.php
+++ src/MessageHandler/WhatsappNotificationHandler.php
@@ -7,19 +7,74 @@ use App\Message\WhatsappNotification;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use App\Entity\Message;
+use App\Service\MessageService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[AsMessageHandler]
 class WhatsappNotificationHandler
 {
+
+
     private $logger;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger,EntityManagerInterface $em, MessageService $service)
     {
         $this->logger = $logger;
+        $this->messageService = $service;
+        $this->entityManager = $em;
     }
 
     public function __invoke(WhatsappNotification $message)
     {
         $this->logger->info("Message sent!");
     }
+
+    public function saveMessageData()
+    {
+        try{
+          $randomNumber = rand(100000,999999);
+       
+
+      $contact = '{"contact":{"profile":{"name":"ankiee"},"wa_id":"34622814642"},"message":{"from":"34622814642","id":"ABGGNGIoFGQvAgo-sAr3kcI5DI30","text":{"body":'.$randomNumber.'},"timestamp":"1640174341","type":"text"}}';
+
+      $data = json_decode($contact, true);
+     /* print '<pre>';
+      print_r($data);*/
+
+      $text = $data['message']['text']['body'];
+      $name =    $data['contact']['profile']['name'];
+      $phoneNumber = $data['message']['from'];
+      $time = $data['message']['timestamp'];
+      $type = $data['message']['type'];
+      $orderId = $data['message']['id'];
+
+
+
+      $message = new message();
+      $entityManager = $this->entityManager;
+      $message->setName($name);
+      $message->setMessage($text);
+      $message->setPhoneNumber($phoneNumber);
+      $message->setOrderId($orderId);
+      $message->setCreatedAt($time);
+      $message->setType($type);
+      $entityManager->persist($message);
+      $entityManager->flush();
+
+      $this->messageService->sendWhatsAppText("34622814642","Hi there");
+
+    
+        return array('status'=>'true');
+    }catch(Exception $e) {
+      
+      return  array('status' => 'false','message'=>$e->getMessage());
+        
+   
+   }
+
+    }
+
+
 }
